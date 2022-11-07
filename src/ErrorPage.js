54b24d66d9988e32f9b4c03b06736898bf314f10
kperusi <106285754+kperusi@users.noreply.github.com>
2022-11-07 08:19:03 +0100

    1 file modified, 4 deleted

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from './404.svg'
+// import logo from './404.svg'
 
 
 function ErrorPage() {
@@ -7,7 +7,7 @@ function ErrorPage() {
     <div className='error-page'>ErrorPage
         <h1>Error 404</h1>
         <h2>Page is not found. Please check your URL</h2>
-        <img src={logo} alt="errorimage" style={{width:'500px', height:'300px'}}/>
+        {/* <img src={logo} alt="errorimage" style={{width:'500px', height:'300px'}}/> */}
     </div>
   )
 }
